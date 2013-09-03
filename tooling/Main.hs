34ac91da1edab6d036e44476e79e218763341949
Jan de Muijnck-Hughes <jfdm@st-andrews.ac.uk>
2013-09-03 15:10:19 +0100

    Forgot to update Main

--- tooling/Main.hs
+++ tooling/Main.hs
@@ -5,11 +5,13 @@ import System.IO
 import Parser
 import Model
 import Transform.Dot
+import Prettifier
 
 main :: IO ()
 main = do args <- getArgs 
           content <- readFile $ head args
           let ast = parseSif content
+--          print $ prettyPlang ast
           putStr $ unlines (plang2Dot ast)
 
 
